ebe442d8db45aada98158328b0cd1fe32a04542a
Mark Bastian <markbastian@gmail.com>
2019-11-25 10:22:41 -0700

    Getting serverless ready

--- project.clj
+++ project.clj
@@ -7,7 +7,8 @@
    [com.andrewmcveigh/cljs-time "0.5.2"]
    [haslett "0.1.6"]
    [cljs-ajax "0.8.0"]
-   [hiccup-bridge "1.0.1"]]
+   [hiccup-bridge "1.0.1"]
+   [markbastian/partsbin "0.1.2"]]
 
   :plugins [[hiccup-bridge "1.0.1"]]
 
